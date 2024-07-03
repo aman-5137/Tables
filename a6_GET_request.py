ed7d8bb5729ed6152a16920a715a80a4a65f622c
aman-5137 <amankkumaryadav5137@gmail.com>
2024-07-03 14:40:28 +0530

    testing tables

---
+++ a6_GET_request.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+# Print the table of any number
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def Table():
+    if request.method == 'POST':
+        num = request.form.get('num')
+        tableList = list()
+        for i in range(1, 11):
+            tableList.append(f"{num} X {i} = {int(num) * i}")
+        return render_template('table.html', tableList = tableList)
+    
+    return render_template('table.html')
